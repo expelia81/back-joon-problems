7a4a33475be9d90d7a7a21fba7ae370d0d353869
김창훈 <expelia81@gmail.com>
2024-07-03 12:28:53 +0900

    [Bronze V] Title: 윤년, Time: 68 ms, Memory: 11588 KB -BaekjoonHub

---
+++ 백준/Bronze/2753. 윤년/윤년.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.BitSet;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		int n = Integer.parseInt(br.readLine());
+
+
+		bw.write((n%4==0 && n%100!=0) || n%400==0? "1":"0");
+
+
+
+		bw.flush();
+		br.close();
+		bw.close();
+	}
+}
