382c3db0e214090d6a0d0cc93a8135c9c3022698
김창훈 <expelia81@gmail.com>
2024-07-05 13:02:30 +0900

    [Bronze V] Title: 사분면 고르기, Time: 80 ms, Memory: 11568 KB -BaekjoonHub

---
+++ 백준/Bronze/14681. 사분면 고르기/사분면 고르기.java
@@ -0,0 +1,31 @@
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		int n = Integer.parseInt(br.readLine());
+		int m = Integer.parseInt(br.readLine());
+
+		if (n>0 && m>0) {
+			System.out.println("1");
+		}
+		if (n>0 && m<0) {
+			System.out.println("4");
+		}
+		if (n<0 && m<0) {
+			System.out.println("3");
+		}
+		if (n<0 && m>0) {
+			System.out.println("2");
+		}
+
+		bw.flush();
+		br.close();
+		bw.close();
+	}
+}
