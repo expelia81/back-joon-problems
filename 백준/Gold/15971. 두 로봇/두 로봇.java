30650ddea2ff6d8e1c1c2596c71db24755a9738a
김창훈 <expelia81@gmail.com>
2024-07-06 10:20:54 +0900

    [Gold IV] Title: 두 로봇, Time: 568 ms, Memory: 80064 KB, Score: 100 point -BaekjoonHub

--- 백준/Gold/15971. 두 로봇/두 로봇.java
+++ 백준/Gold/15971. 두 로봇/두 로봇.java
@@ -1,5 +1,6 @@
 
 import java.io.*;
+import java.util.ArrayList;
 import java.util.BitSet;
 import java.util.List;
 import java.util.StringTokenizer;
@@ -8,7 +9,9 @@ public class Main {
 
 
 
-	private static int[][] arr;
+
+//	private static int[][] arr;
+	private static List<List<int[]>> arr = new ArrayList<>();
 
 	private static int result=Integer.MAX_VALUE;
 	public static void main(String[] args) throws IOException {
@@ -23,18 +26,26 @@ public class Main {
 		int a = Integer.parseInt(st.nextToken());
 		int b = Integer.parseInt(st.nextToken());
 
-		arr=new int[n+1][n+1];
+//		arr=new int[n+1][n+1];
+		for (int i = 0; i <= n; i++) {
+			arr.add(new ArrayList<>());
+		}
 
 		/* 배열 필요한 경우 */
 		for (int i = 1; i < n; i++) {
 			st=new StringTokenizer(br.readLine()," ");
 			int left = Integer.parseInt(st.nextToken());
 			int right = Integer.parseInt(st.nextToken());
+			int value = Integer.parseInt(st.nextToken());
 
-			arr[left][right]=arr[right][left]=Integer.parseInt(st.nextToken());
+//			arr[left][right]=arr[right][left]=Integer.parseInt(st.nextToken());
+			arr.get(left).add(new int[]{right,value});
+			arr.get(right).add(new int[]{left,value});
 		}
 
-		dfs(a,0,0,new BitSet(n+1), b);
+		BitSet bitSet = new BitSet(n+1);
+		bitSet.set(a);
+		dfs(a,0,0,bitSet, b);
 
 		bw.write(result+"");
 
@@ -51,13 +62,21 @@ public class Main {
 			result=Math.min(sum-max,result);
 			return;
 		}
-		isVisited.set(index);
 
-		for (int i = 1; i < arr.length; i++) {
-			if (arr[index][i]!=0 && !isVisited.get(i)) {
-				BitSet bitSet = (BitSet) isVisited.clone();
-				dfs(i, Math.max(max,arr[index][i]),sum+arr[index][i],bitSet,target);
+		for (int[] temp : arr.get(index)) {
+			int i = temp[0];
+			int val = temp[1];
+			if (!isVisited.get(i)) {
+				isVisited.set(i);
+				dfs(i, Math.max(max,val),sum+val,isVisited,target);
+				isVisited.set(i, false);
 			}
 		}
+//		for (int i = 1; i < arr.length; i++) {
+//			if (arr[index][i]!=0 && !isVisited.get(i)) {
+//				dfs(i, Math.max(max,arr[index][i]),sum+arr[index][i],isVisited,target);
+//				isVisited.set(i,false);
+//			}
+//		}
 	}
 }
