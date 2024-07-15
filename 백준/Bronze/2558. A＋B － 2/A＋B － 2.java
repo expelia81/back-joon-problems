6158111600a2d3c254289ecdabcb60fca81afe35
김창훈 <expelia81@gmail.com>
2024-07-15 13:39:20 +0900

    [Bronze V] Title: A+B - 2, Time: 76 ms, Memory: 11588 KB -BaekjoonHub

---
+++ 백준/Bronze/2558. A＋B － 2/A＋B － 2.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		int n = Integer.parseInt(br.readLine());
+		int n2 = Integer.parseInt(br.readLine());
+
+//		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+//
+//		int x = Integer.parseInt(st.nextToken());
+//		int y = Integer.parseInt(st.nextToken());
+		bw.write(String.valueOf(n+n2));
+
+		bw.flush();
+		br.close();
+		bw.close();
+	}
+}
