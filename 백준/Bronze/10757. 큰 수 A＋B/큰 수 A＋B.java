d8d049c0dec1e8609625f33af6e5696af87bf8da
김창훈 <expelia81@gmail.com>
2024-07-24 23:05:51 +0900

    [Bronze V] Title: 큰 수 A+B, Time: 196 ms, Memory: 15140 KB -BaekjoonHub

---
+++ 백준/Bronze/10757. 큰 수 A＋B/큰 수 A＋B.java
@@ -0,0 +1,27 @@
+
+import java.io.*;
+import java.math.BigInteger;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		BigInteger integer = new BigInteger(st.nextToken());
+		BigInteger integer2 = new BigInteger(st.nextToken());
+
+		System.out.println(integer.add(integer2));
+
+
+
+
+		bw.flush();
+		br.close();
+		bw.close();
+	}
+}
