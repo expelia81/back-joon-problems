c378ac50e11fb3763a236c2b561ca6e0a8b64cae
김창훈 <expelia81@gmail.com>
2024-07-28 23:37:05 +0900

    [Bronze V] Title: 飴の袋詰め (Drops Packing), Time: 76 ms, Memory: 11560 KB -BaekjoonHub

---
+++ 백준/Bronze/31610. 飴の袋詰め （Drops Packing）/飴の袋詰め （Drops Packing）.java
@@ -0,0 +1,30 @@
+
+import java.io.*;
+import java.math.BigInteger;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+//		StringTokenizer st = new StringTokenizer(br.readLine());
+//
+//		int result = Integer.parseInt(st.nextToken()) + Integer.parseInt(st.nextToken());
+//		int inputResult = Integer.parseInt(st.nextToken());
+
+		int h = Integer.parseInt(br.readLine());
+		int m = Integer.parseInt(br.readLine());
+		int c = Integer.parseInt(br.readLine());
+
+		bw.write((h*m)+c+"");
+
+
+				bw.flush();
+				br.close();
+				bw.close();
+			}
+
+}
