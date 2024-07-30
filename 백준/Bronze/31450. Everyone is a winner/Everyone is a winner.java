e61ab48fd066f02ef5409b740ee51d50960a9981
김창훈 <expelia81@gmail.com>
2024-07-30 22:17:09 +0900

    [Bronze V] Title: Everyone is a winner, Time: 80 ms, Memory: 11584 KB -BaekjoonHub

---
+++ 백준/Bronze/31450. Everyone is a winner/Everyone is a winner.java
@@ -0,0 +1,31 @@
+
+import java.io.*;
+import java.math.BigInteger;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		int a = Integer.parseInt(st.nextToken());
+		int b = Integer.parseInt(st.nextToken());
+//
+//		int result = Integer.parseInt(st.nextToken()) + Integer.parseInt(st.nextToken());
+//		int inputResult = Integer.parseInt(st.nextToken());
+
+
+		bw.write(a % b ==0 ? "Yes" : "No");
+
+
+
+		bw.flush();
+		br.close();
+		bw.close();
+	}
+
+}
