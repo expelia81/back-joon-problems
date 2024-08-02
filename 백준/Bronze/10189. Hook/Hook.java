fcc17907e9adea7327ead65a4f3ff4313f9d6f70
김창훈 <expelia81@gmail.com>
2024-08-02 11:13:03 +0900

    [Bronze V] Title: Hook, Time: 120 ms, Memory: 13988 KB -BaekjoonHub

---
+++ 백준/Bronze/10189. Hook/Hook.java
@@ -0,0 +1,40 @@
+import java.io.*;
+import java.math.BigInteger;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+//		StringTokenizer st = new StringTokenizer(br.readLine());
+
+//		int a = Integer.parseInt(st.nextToken());
+//		int b = Integer.parseInt(st.nextToken());
+//
+//		int result = Integer.parseInt(st.nextToken()) + Integer.parseInt(st.nextToken());
+//		int inputResult = Integer.parseInt(st.nextToken());
+
+		//이 별은 무슨색일까
+
+
+		String s = """
+						#  # #### #### #  #
+						#### #  # #  # # #
+						#### #  # #  # # #
+						#  # #### #### #  #
+						""";
+		bw.write(s);
+
+
+
+
+
+		bw.flush();
+		br.close();
+		bw.close();
+	}
+
+}
