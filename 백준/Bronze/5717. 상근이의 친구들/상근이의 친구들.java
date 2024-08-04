e4f99bda9210e6f180c11b551cc78ef97f55b4fe
김창훈 <expelia81@gmail.com>
2024-08-05 01:19:16 +0900

    [Bronze IV] Title: 상근이의 친구들, Time: 60 ms, Memory: 11636 KB -BaekjoonHub

---
+++ 백준/Bronze/5717. 상근이의 친구들/상근이의 친구들.java
@@ -0,0 +1,29 @@
+
+import java.io.*;
+import java.math.BigInteger;
+import java.util.*;
+
+public class Main {
+
+	public static void main(String [] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+
+		while (true) {
+			StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+			int n = Integer.parseInt(st.nextToken());
+			int m = Integer.parseInt(st.nextToken());
+			if (n==0 && m==0) break;
+			bw.write((n+m)+"\n");
+		}
+
+
+		bw.flush();
+		br.close();
+		bw.close();
+	}
+
+}
