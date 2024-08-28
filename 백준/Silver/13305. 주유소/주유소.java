79678998483c490dfe96b744fc61371ea5d14efd
김창훈 <expelia81@gmail.com>
2024-08-28 21:51:00 +0900

    [Silver III] Title: 주유소, Time: 208 ms, Memory: 31128 KB, Score: 58 point -BaekjoonHub

---
+++ 백준/Silver/13305. 주유소/주유소.java
@@ -0,0 +1,53 @@
+
+import java.io.*;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		int n = Integer.parseInt(br.readLine());
+
+
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		StringTokenizer st2 = new StringTokenizer(br.readLine());
+
+		int[] meters = new int[n-1];
+		int[] prices = new int[n-1];
+
+		for (int i = 0; i < n-1; i++) {
+			meters[i]=Integer.parseInt(st.nextToken());
+			prices[i]=Integer.parseInt(st2.nextToken());
+		}
+
+		int nowPrice = prices[0];
+		int meter = meters[0];
+
+		int result = 0;
+		for (int i = 1; i < n-1; i++) {
+			if (prices[i]<nowPrice) {
+				result += meter*nowPrice;
+				nowPrice = prices[i];
+				meter = 0;
+			}
+			meter += meters[i];
+		}
+
+		result += meter*nowPrice;
+		System.out.println(result);
+
+
+
+
+
+
+		bw.flush();
+		br.close();
+		bw.close();
+	}
+
+}
