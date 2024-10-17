7e0b6349f6f307ea6376ca0c7f54f385c9308ff2
김창훈 <expelia81@gmail.com>
2024-10-17 23:35:50 +0900

    [Bronze V] Title: 웰컴, Time: 64 ms, Memory: 11780 KB -BaekjoonHub

---
+++ 백준/Bronze/5337. 웰컴/웰컴.kt
@@ -0,0 +1,20 @@
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
+import java.util.*
+
+fun main() {
+
+    val br: BufferedReader = BufferedReader(InputStreamReader(System.`in`))
+    val bw: BufferedWriter = BufferedWriter(OutputStreamWriter(System.out))
+
+    println(".  .   .\n" +
+            "|  | _ | _. _ ._ _  _\n" +
+            "|/\\|(/.|(_.(_)[ | )(/.")
+
+
+    bw.flush()
+    br.close()
+    bw.close()
+}
