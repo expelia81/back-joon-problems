fe575bdda9a6e0d1af028697aff08f1023856df5
김창훈 <expelia81@gmail.com>
2025-02-13 19:34:05 +0900

    [Bronze IV] Title: 빠른 A+B, Time: 712 ms, Memory: 259636 KB -BaekjoonHub

---
+++ 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.kt
@@ -0,0 +1,24 @@
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
+import java.util.*
+
+fun main() {
+    val br: BufferedReader = BufferedReader(InputStreamReader(System.`in`))
+    val bw: BufferedWriter = BufferedWriter(OutputStreamWriter(System.out))
+
+    var n = br.readLine().toInt()
+
+    repeat(n) {
+        var st= StringTokenizer(br.readLine(), " ")
+
+        var m = st.nextToken().toInt()
+        var k = st.nextToken().toInt()
+        bw.write("${m+k}\n")
+    }
+
+    bw.flush()
+    br.close()
+    bw.close()
+}
