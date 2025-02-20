24a79dbfe482b8141e4fef120a0bb6bf08d56c23
김창훈 <expelia81@gmail.com>
2025-02-20 20:44:42 +0900

    [Bronze V] Title: 아스키 코드, Time: 76 ms, Memory: 11968 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.kt
@@ -0,0 +1,22 @@
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
+import java.util.*
+
+fun main() {
+    val br: BufferedReader = BufferedReader(InputStreamReader(System.`in`))
+    val bw: BufferedWriter = BufferedWriter(OutputStreamWriter(System.out))
+
+    var n = br.readLine().get(0)
+
+    bw.write(n.toInt().toString())
+
+
+
+
+
+    bw.flush()
+    br.close()
+    bw.close()
+}
